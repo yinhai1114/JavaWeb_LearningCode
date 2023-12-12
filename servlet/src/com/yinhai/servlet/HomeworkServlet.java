c92bb4d1ab7d668a721e515ed17243b576498411
银小海 <643013242@QQ.COM>
2023-12-12 17:53:12 +0800

    HttpServletRequest的作业01

---
+++ servlet/src/com/yinhai/servlet/HomeworkServlet.java
@@ -0,0 +1,24 @@
+package com.yinhai.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author 银小海
+ * @version 1.0
+ * @email yinhai14@qq.com
+ */
+@WebServlet(name = "Homework01",urlPatterns = "/homeworkServlet")
+public class HomeworkServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+}
