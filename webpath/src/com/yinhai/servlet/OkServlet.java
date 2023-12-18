98e76f547b951e210181cd13f0823e479c78bd7d
银小海 <643013242@QQ.COM>
2023-12-18 19:03:44 +0800

    Web的工程路径 的一个servlet类

---
+++ webpath/src/com/yinhai/servlet/OkServlet.java
@@ -0,0 +1,20 @@
+package com.yinhai.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+/**
+ * @author 银小海
+ * @version 1.0
+ * @email yinhai14@qq.com
+ */
+@WebServlet(name = "OkServlet",urlPatterns = "/ok")
+public class OkServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        System.out.println("OkServlet be invoked....");
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doPost(request,response);
+    }
+}
