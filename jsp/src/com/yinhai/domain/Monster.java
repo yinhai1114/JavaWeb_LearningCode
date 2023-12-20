fe6bb54942856e51bb46c6c51bd0b73478845962
银小海 <643013242@QQ.COM>
2023-12-20 22:29:29 +0800

    两个用于展示域对象容器存入对象

---
+++ jsp/src/com/yinhai/domain/Monster.java
@@ -0,0 +1,43 @@
+package com.yinhai.domain;
+
+/**
+ * @author 银小海
+ * @version 1.0
+ * @email yinhai14@qq.com
+ */
+public class Monster {
+    private Integer id;
+    private String name;
+    private String skill;
+
+    public Monster(Integer id, String name, String skill) {
+        this.id = id;
+        this.name = name;
+        this.skill = skill;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+}
+
