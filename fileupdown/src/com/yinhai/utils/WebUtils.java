8a3425137424c7ddff9fc3bdc25868f5a49377fa
银小海 <643013242@QQ.COM>
2023-12-26 23:05:38 +0800

    一个工具类 用于处理当前时间供servlet创建文件夹使用

---
+++ fileupdown/src/com/yinhai/utils/WebUtils.java
@@ -0,0 +1,24 @@
+package com.yinhai.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+public class WebUtils {
+
+
+    public static String getYearMonthDay() {
+        //如何得到当前的日期-> java基础 日期 三代类
+        LocalDateTime ldt = LocalDateTime.now();
+        int year = ldt.getYear();
+        int monthValue = ldt.getMonthValue();
+        int dayOfMonth = ldt.getDayOfMonth();
+        String yearMonthDay = year + "-" + monthValue + "-" + dayOfMonth + "/";
+        System.out.println(yearMonthDay);
+        return yearMonthDay;
+    }
+    // 技巧, 最好测试一把
+    // public static void main(String[] args) {
+    //    System.out.println(WebUtils.getYearMonthDay());
+    // }
+}
