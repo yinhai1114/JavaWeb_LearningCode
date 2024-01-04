9206be50c01605e26a0bc21aa95bf6ac015b43a9
银小海 <643013242@QQ.COM>
2024-01-04 20:56:12 +0800

    Service测试

---
+++ jiaju_mall/src/com/yinhai/furns/test/MemberServiceTest.java
@@ -0,0 +1,35 @@
+package com.yinhai.furns.test;
+
+import com.yinhai.furns.javabean.Member;
+import com.yinhai.furns.service.MemberService;
+import com.yinhai.furns.service.impl.MemberServiceImpl;
+import com.yinhai.furns.utils.JDBCUtilsByDruid;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 银小海
+ * @version 1.0
+ * @email yinhai14@qq.com
+ */
+public class MemberServiceTest {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        if (memberService.isExistsUsername("admin")){
+            System.out.println("用户存在");
+        }else{
+            System.out.println("用户不存在");
+        }
+    }
+    @Test
+    public void test(){
+        Member member = new Member(null, "tom", "tom", "643013242@qq.com");
+        MemberService memberService = new MemberServiceImpl();
+        boolean b = memberService.registerMember(member);
+        if (b){
+            System.out.println("注册成功");
+        }else{
+            System.out.println("注册失败");
+        }
+        JDBCUtilsByDruid.commit();
+    }
+}
