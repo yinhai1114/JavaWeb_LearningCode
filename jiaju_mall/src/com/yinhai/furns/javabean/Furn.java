dbafc178c36e3b5ee6b8079f8205be70eb3d4617
银小海 <643013242@QQ.COM>
2024-01-07 15:45:36 +0800

    Furn的bean类

---
+++ jiaju_mall/src/com/yinhai/furns/javabean/Furn.java
@@ -0,0 +1,103 @@
+package com.yinhai.furns.javabean;
+
+import java.math.BigDecimal;
+
+/**
+ * Furn和mysql表进行映射
+ * @author 银小海
+ * @version 1.0
+ * @email yinhai14@qq.com
+ */
+public class Furn {
+    // private int id;//不能用iNt 要用Integer
+    private Integer id;
+    private String name;
+    private String maker;
+    private BigDecimal price;
+    private Integer sales;
+    private Integer stock;
+    //表的字段是imgPath 而bean用的imgPath 如何解决
+    private String imgPath;
+
+    public Furn(Integer id, String name, String maker, BigDecimal price, Integer sales, Integer stock, String imgPath) {
+        this.id = id;
+        this.name = name;
+        this.maker = maker;
+        this.price = price;
+        this.sales = sales;
+        this.stock = stock;
+        this.imgPath = imgPath;
+    }
+
+    public Furn() {
+    }
+
+    @Override
+    public String toString() {
+        return "Furn{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", maker='" + maker + '\'' +
+                ", price=" + price +
+                ", sales=" + sales +
+                ", stock=" + stock +
+                ", imgPath='" + imgPath + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getSales() {
+        return sales;
+    }
+
+    public void setSales(Integer sales) {
+        this.sales = sales;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public String getImgPath() {
+        return imgPath;
+    }
+
+    public void setImgPath(String imgPath) {
+        this.imgPath = imgPath;
+    }
+}
