17735ee8cb418eab263c9b8b9315c4866b177f3c
银小海 <643013242@QQ.COM>
2024-01-07 15:46:17 +0800

    Furn的服务 提供DAO的使用

---
+++ jiaju_mall/src/com/yinhai/furns/service/FurnService.java
@@ -0,0 +1,14 @@
+package com.yinhai.furns.service;
+
+import com.yinhai.furns.javabean.Furn;
+
+import java.util.List;
+
+/**
+ * @author 银小海
+ * @version 1.0
+ * @email yinhai14@qq.com
+ */
+public interface FurnService {
+    public List<Furn> queryFurns();
+}
