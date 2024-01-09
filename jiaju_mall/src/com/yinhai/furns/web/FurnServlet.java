851bdb72d16c6b037bb0b0c3fd93b0c799da7a88
银小海 <643013242@QQ.COM>
2024-01-09 21:20:35 +0800

    Furn功能增加 使用一个DataUtils 自动封装javaBean类

--- jiaju_mall/src/com/yinhai/furns/web/FurnServlet.java
+++ jiaju_mall/src/com/yinhai/furns/web/FurnServlet.java
@@ -4,6 +4,8 @@ import com.yinhai.furns.dao.BasicDAO;
 import com.yinhai.furns.javabean.Furn;
 import com.yinhai.furns.service.FurnService;
 import com.yinhai.furns.service.impl.FurnServiceImpl;
+import com.yinhai.furns.utils.DataUtils;
+import org.apache.commons.beanutils.BeanUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -44,13 +46,61 @@ public class FurnServlet extends BasicServlet {
     }
     protected  void add(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
         //获取家居信息
-        String uname = request.getParameter("name");
-        String maker = request.getParameter("maker");
-        String price = request.getParameter("price");
-        String sales = request.getParameter("sales");
-        String stock = request.getParameter("stock");
-        String defaultPath = "assets/images/product-image/default.jpg";
-        Furn furn = new Furn(null, uname, maker, new BigDecimal(price), new Integer(sales), new Integer(stock), defaultPath);
+        // String uname = request.getParameter("name");
+        // String maker = request.getParameter("maker");
+        // String price = request.getParameter("price");
+        // String sales = request.getParameter("sales");
+        //
+        //
+        // String stock = request.getParameter("stock");
+        // String defaultPath = "assets/images/product-image/default.jpg";
+        // Furn furn = new Furn(null, uname, maker, new BigDecimal(price), new Integer(sales), new Integer(stock), defaultPath);
+        //我们可以对获取的到数据, 进行一个校验
+        //1. 使用java的正则表达式来验证 sales是一个正整数
+        //2. 如果没有通过校验，则直接返回furn_add.jsp -> request.setAttribute("mes","xx")
+        //3. 这里可以直接进行转换
+        //try {
+        //    int i = Integer.parseInt(sales);
+        //}catch (NumberFormatException e) {
+        //    //System.out.println("转换异常...");
+        //    req.setAttribute("mes", "销量数据格式不对...");
+        //    //返回到furn_add.jsp
+        //    req.getRequestDispatcher("/views/manage/furn_add.jsp")
+        //            .forward(req, resp);
+        //    return;
+        //}
+
+        //String stock = req.getParameter("stock");
+        //图片的路径 imgPath 使用默认即可
+        //Furn furn = null;
+        //try {
+        //    furn = new Furn(null, name, maker, new BigDecimal(price),
+        //            new Integer(sales), new Integer(stock), "assets/images/product-image/default.jpg");
+        //} catch (NumberFormatException e) {
+        //    req.setAttribute("mes", "添加数据格式不对...");
+        //    //返回到furn_add.jsp
+        //    req.getRequestDispatcher("/views/manage/furn_add.jsp")
+        //            .forward(req, resp);
+        //    return;
+        //}
+
+        //后面我们会学习SpringMVC -> 专门的用于数据校验的规则/框架 JSR303... Hibernate Validator
+        //这里我们使用第二种方式, 完成将前端提交的数据， 封装成Furn的Javabean对象
+        //使用BeanUtils完成javabean对象的自动封装.
+        //
+        // Furn furn = new Furn();
+        // try {
+        //    //讲 req.getParameterMap() 数据封装到furn 对象
+        //    //使用反射将数据封装, 有一个前提就是表单提交的数据字段名
+        //    //<input name="maker" style="width: 90%" type="text" value=""/>
+        //    //需要和封装的Javabean的属性名一致
+        //    BeanUtils.populate(furn, request.getParameterMap());
+        // } catch (Exception e) {
+        //    e.printStackTrace();
+        // }
+        // 自动将提交的数据，封装到Furn对象
+        Furn furn =
+                DataUtils.copyParamToBean(request.getParameterMap(), new Furn());
         System.out.println(furn);
         furnService.addFurn(furn);
 
