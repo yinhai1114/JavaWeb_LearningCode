3a72d12b45f53509b15ea7c293b6e381d1b9364b
银小海 <643013242@QQ.COM>
2024-01-09 21:20:25 +0800

    Furn功能增加 使用一个DataUtils 自动封装javaBean类

--- jiaju_mall/src/com/yinhai/furns/javabean/Furn.java
+++ jiaju_mall/src/com/yinhai/furns/javabean/Furn.java
@@ -17,7 +17,7 @@ public class Furn {
     private Integer sales;
     private Integer stock;
     //表的字段是imgPath 而bean用的imgPath 如何解决
-    private String imgPath;
+    private String imgPath = "assets/images/product-image/default.jpg";
 
     public Furn(Integer id, String name, String maker, BigDecimal price, Integer sales, Integer stock, String imgPath) {
         this.id = id;
@@ -26,7 +26,9 @@ public class Furn {
         this.price = price;
         this.sales = sales;
         this.stock = stock;
-        this.imgPath = imgPath;
+        if(!(null == imgPath || "".equals(imgPath))){
+            this.imgPath = imgPath;
+        }
     }
 
     public Furn() {
