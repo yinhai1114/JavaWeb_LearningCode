2f6a6e3ded948633a49bac61be7baca2ac3026bf
银小海 <643013242@QQ.COM>
2024-01-10 19:10:23 +0800

    service类增加创建page的方法

--- jiaju_mall/src/com/yinhai/furns/service/impl/FurnServiceImpl.java
+++ jiaju_mall/src/com/yinhai/furns/service/impl/FurnServiceImpl.java
@@ -3,6 +3,7 @@ package com.yinhai.furns.service.impl;
 import com.yinhai.furns.dao.FurnDAO;
 import com.yinhai.furns.dao.impl.FurnDAOImpl;
 import com.yinhai.furns.javabean.Furn;
+import com.yinhai.furns.javabean.Page;
 import com.yinhai.furns.service.FurnService;
 
 import java.util.List;
@@ -27,5 +28,47 @@ public class FurnServiceImpl implements FurnService {
     @Override
     public int addFurn(Furn furn) {
         return furnDAO.addFurn(furn);
+    }
+
+    @Override
+    public int deleteFurnById(int id) {
+        return furnDAO.deleteFurnById(id);
+    }
+
+    @Override
+    public Furn queryFurnById(int id) {
+        return furnDAO.queryFurnById(id);
+    }
+
+    @Override
+    public int updateFurn(Furn furn) {
+        return furnDAO.updateFurn(furn);
+    }
+
+    @Override
+    public Page<Furn> page(int pageNo, int pageSize) {
+        Page<Furn> page = new Page<>();
+        page.setPageNo(pageNo);
+        page.setPageSize(pageSize);
+        int totalRow = furnDAO.getTotalRow();
+        page.setTotalRow(totalRow);
+        //pageTotalCount最大页数 需要通过算法计算得到
+        int pageTotalCount = totalRow / pageSize;
+        if(totalRow % pageSize > 0){
+            pageTotalCount += 1;
+        }
+        page.setPageTotalCount(pageTotalCount);
+        //private List<T> items
+        //计算begin
+        //private List<T> items
+        //老师开始计算begin-> 小小算法
+        //验证: pageNo = 1 pageSize = 3 => begin =0
+        //验证: pageNo = 3 pageSize = 2 => begin =4
+        //OK => 但是注意这里隐藏一个坑, 现在你看不到, 后面会暴露
+        int begin = (pageNo - 1) * pageSize;
+        List<Furn> pageItems = furnDAO.getPageItems(begin, pageSize);
+        page.setItems(pageItems);
+        //还差一个url => 分页导航，先放一放
+        return page;
     }
 }
