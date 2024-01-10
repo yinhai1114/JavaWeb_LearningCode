63ac63096ce11dca4d64ec5b68f71666b5ccb1d4
银小海 <643013242@QQ.COM>
2024-01-10 19:10:38 +0800

    service类增加创建page的接口抽象方法

--- jiaju_mall/src/com/yinhai/furns/service/FurnService.java
+++ jiaju_mall/src/com/yinhai/furns/service/FurnService.java
@@ -1,6 +1,7 @@
 package com.yinhai.furns.service;
 
 import com.yinhai.furns.javabean.Furn;
+import com.yinhai.furns.javabean.Page;
 
 import java.util.List;
 
@@ -12,4 +13,14 @@ import java.util.List;
 public interface FurnService {
     public List<Furn> queryFurns();
     public int addFurn(Furn furn);
+    public int deleteFurnById(int id);
+    public Furn queryFurnById(int id);
+    public int updateFurn(Furn furn);
+    /**
+     * 根据传入的pageNo和 pageSize , 返回对应的page对象
+     * @param pageNo
+     * @param pageSize
+     * @return
+     */
+    public Page<Furn> page(int pageNo,int pageSize);
 }
